ce61f2547f0c3f22877af139296d5bf4cd944a3c
urmothergei <chiaminghan123@gmail.com>
2025-02-01 22:53:44 +0800

    updated

--- frontend/App.js
+++ frontend/App.js
@@ -42,3 +42,11 @@ export default function App() {
     </NavigationContainer>
   );
 }
+
+
+    /* JSON SCRIPTS ORIGINAL
+    "start": "react-scripts start",
+    "build": "react-scripts build",
+    "test": "react-scripts test",
+    "eject": "react-scripts eject"
+    */
